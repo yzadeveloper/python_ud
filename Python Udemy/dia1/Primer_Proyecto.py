d3a5471453207f9e8e6e8260f29633808e5ce454
alfaryolanda79 <alfaroyolanda@hotmail.com>
2023-10-31 20:26:18 +0100

    first commit

---
+++ Python Udemy/dia1/Primer_Proyecto.py
@@ -0,0 +1,2 @@
+#escribir un programa que cree un nombre comercial en base a lo que el usuario escriba
+print("El nombre de tu cerveza es: \n" + "\'" + input("Dime cómo se llama tu mascota: ") + input("Dime cuál es tu fase lunar preferida : ") + "\'")

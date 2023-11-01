d824de58c6aa37f59823232f6e6f3161b4c87804
alfaryolanda79 <alfaroyolanda@hotmail.com>
2023-11-01 19:58:04 +0100

    coursera variables

---
+++ coursera.py
@@ -0,0 +1,13 @@
+print(type(2))
+print(type(2.5))
+# <class 'tuple'>
+print(type((2, 6, 9)))
+# <class 'list'>
+print(type([2, 7, 9]))
+# variables
+base = 3
+altura = 2
+area = (base * altura)/2
+print(area)
+# para poder imprir area junto a un string se convierte a string con str()
+print("El área es " + str(area))
